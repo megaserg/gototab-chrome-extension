0ea63c700bf65e17ccfa882359d9412ae4338409
Sergey Serebryakov <sserebryakov@twitter.com>
2015-04-27 02:24:20 -0700

    Replace * glob (powershell ಠ_ಠ)

--- spec/popupSpec.js
+++ spec/popupSpec.js
@@ -3,7 +3,6 @@ describe("FuzzySearch", function() {
   var fuzzy = require('../out/FuzzySearch.js');
 
   it("should be able to rank items", function() {
-    console.log(fuzzy);
     var scorer = new fuzzy.FuzzySearch("avi");
     var results = ["Hello world!", "Travis", "Appveyor"].sort(function(a, b) {
         return scorer.score(b) - scorer.score(a);
